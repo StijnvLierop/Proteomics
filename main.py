505755e4a74f784bd173358251b0ef32c1f7510c
Stijn van Lierop <150940112+StijnvLierop@users.noreply.github.com>
2024-03-27 15:57:17 +0100

    added header text

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
 from typing import Iterable
-
 import numpy as np
 import pandas as pd
 import streamlit as st
 
-# Define body fluids
+# Define body fluid names
 BODY_FLUIDS = ["saliva", "semen", "vaginalfluid", "urine", "blood"]
 
-# Read data
-# po_df = pd.read_csv("2581_PureOnly_Report_25MAR24_NP.csv", sep=';')
 def add_protein_count_per_body_fluid(df: pd.DataFrame,
                                      samples_to_exclude: Iterable[str] = None) -> pd.DataFrame:
     # Define samples to look at
@@ -34,10 +31,6 @@ def add_protein_count_per_body_fluid(df: pd.DataFrame,
     return df
 
 
-def get_discriminative_proteins(df: pd.DataFrame):
-    return
-
-
 if __name__ == '__main__':
     # Set samples to exclude
     samples_to_exclude = None
@@ -55,5 +48,7 @@ if __name__ == '__main__':
         # Add nr of times each protein occurs in a body fluid
         po_df = add_protein_count_per_body_fluid(po_df, samples_to_exclude)
 
+        # Show dataframe
         show_df = po_df[['PG.ProteinDescriptions'] + BODY_FLUIDS]
+        st.write("Nr of samples with protein per body fluid")
         st.write(show_df)
